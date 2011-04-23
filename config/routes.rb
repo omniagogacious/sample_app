848e038586b8272ec1d60b598fcb0badfaeed4cb
Carson Tang <tang.carson@gmail.com>
2011-04-23 14:09:51 -0700

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => "pages#about"
   match '/help',    :to => "pages#help"
-
+  match '/signup',  :to => "users#new"
+  
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => "pages#home"
